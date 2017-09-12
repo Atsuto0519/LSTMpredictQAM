ccebd285236853a1b3bb4c18f3f4d159910255ac
Atsuto0519 <atsuto.fps@gmail.com>
2017-09-12 13:16:40 +0900

    To gpu

--- train_QAM.py
+++ train_QAM.py
@@ -63,7 +63,7 @@ def compute_loss(model, sequences):
 # Nは周波数fcの信号の周期を何分割するか
 N=64
 # 搬送波の周波数
-fc = 1
+fc = 2
 # QAMで信号化したいビット列
 letter=['001','100','000','011','101','110']
 signal=[]
@@ -76,10 +76,10 @@ HIDDEN_UNITS = 5
 OUT_UNITS = 1
 TRAINING_EPOCHS = 4000
 DISPLAY_EPOCH = 10
-MINI_BATCH_SIZE = 100
-LENGTH_OF_SEQUENCE = 100
+MINI_BATCH_SIZE = len(signal)
+LENGTH_OF_SEQUENCE = fc
 STEPS_PER_CYCLE = N
-NUMBER_OF_CYCLES = fc * len(signal)
+NUMBER_OF_CYCLES = fc
 
 xp = cuda.cupy
 
@@ -90,50 +90,50 @@ if __name__ == "__main__":
     train_data = data_maker.make(signal)
 
     plt.title("QAM")
-    plt.ylim(min(qam)-0.2, max(train_data)+0.2)
-    plt.plot(range(len(train_data)),train_data,"o")
+    plt.ylim(min(train_data)-0.2, max(train_data)+0.2)
+    plt.plot(range(len(train_data)),train_data)
 
     plt.show()
 
 
-    # # setup model
-    # model = LSTM(IN_UNITS, HIDDEN_UNITS, OUT_UNITS)
-    # for param in model.params():
-    #     data = param.data
-    #     data[:] = np.random.uniform(-0.1, 0.1, data.shape)
-    #
-    # model.to_gpu()
-    #
-    # # setup optimizer
-    # optimizer = optimizers.Adam()
-    # optimizer.setup(model)
-    #
-    # start = time.time()
-    # cur_start = start
-    # for epoch in range(TRAINING_EPOCHS):
-    #     sequences = data_maker.make_mini_batch(train_data, mini_batch_size=MINI_BATCH_SIZE, length_of_sequence=LENGTH_OF_SEQUENCE)
-    #     model.reset_state()
-    #     model.zerograds()
-    #     loss = compute_loss(model, sequences)
-    #     loss.backward()
-    #     optimizer.update()
-    #
-    #     if epoch != 0 and epoch % DISPLAY_EPOCH == 0:
-    #         cur_end = time.time()
-    #         # display loss
-    #         print(
-    #             "[{j}]training loss:\t{i}\t{k}[sec/epoch]".format(
-    #                 j=epoch,
-    #                 i=loss.data/(sequences.shape[1] - 1),
-    #                 k=(cur_end - cur_start)/DISPLAY_EPOCH
-    #             )
-    #         )
-    #         cur_start = time.time()
-    #         sys.stdout.flush()
-    #
-    # end = time.time()
-    #
-    # # save model
-    # cPickle.dump(model, open("./model.pkl", "wb"))
+    # setup model
+    model = LSTM(IN_UNITS, HIDDEN_UNITS, OUT_UNITS)
+    for param in model.params():
+        data = param.data
+        data[:] = np.random.uniform(-0.1, 0.1, data.shape)
+
+    model.to_gpu()
+
+    # setup optimizer
+    optimizer = optimizers.Adam()
+    optimizer.setup(model)
+
+    start = time.time()
+    cur_start = start
+    for epoch in range(TRAINING_EPOCHS):
+        sequences = data_maker.make_mini_batch(train_data, mini_batch_size=MINI_BATCH_SIZE, length_of_sequence=LENGTH_OF_SEQUENCE)
+        model.reset_state()
+        model.zerograds()
+        loss = compute_loss(model, sequences)
+        loss.backward()
+        optimizer.update()
+
+        if epoch != 0 and epoch % DISPLAY_EPOCH == 0:
+            cur_end = time.time()
+            # display loss
+            print(
+                "[{j}]training loss:\t{i}\t{k}[sec/epoch]".format(
+                    j=epoch,
+                    i=loss.data/(sequences.shape[1] - 1),
+                    k=(cur_end - cur_start)/DISPLAY_EPOCH
+                )
+            )
+            cur_start = time.time()
+            sys.stdout.flush()
+
+    end = time.time()
+
+    # save model
+    cPickle.dump(model, open("./model.pkl", "wb"))
 
     print("{}[sec]".format(end - start))
